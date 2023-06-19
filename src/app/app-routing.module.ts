7efc4edccd9ba40a0a3753c176d2f129c3a89f8f
vanessa <vanessaduquerubio@gmail.com>
2023-06-19 16:53:16 +0200

    cambio de ruta y componente carrito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'carrito', component: CarritoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
