639d97fa7653424e4c5676b4f7bc501fe10f233c
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-06-19 16:46:19 +0200

    creamos la ruta y el array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'productos', component: ListaProductosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-productos',
+  templateUrl: './lista-productos.component.html',
+  styleUrls: ['./lista-productos.component.css']
+})
+export class ListaProductosComponent {
+
+  arrProductos: any[]
+
+
+  constructor() {
+    this.arrProductos = []
+  }
+}
