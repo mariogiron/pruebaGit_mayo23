f0cc18df67cfad43b4054802ac863a635e15c149
ANA CACERES <acaceresrueda@gmail.com>
2023-06-19 17:07:28 +0200

    formulario creado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioProductoComponent } from './components/formulario-producto/formulario-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'productos/new', component: FormularioProductoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
